155abb905fdc9e6ca9694b3f839aee9318190696
GAURANG SINGHAL <108609473+gaurangsinghal15@users.noreply.github.com>
2024-12-07 16:23:20 +0530

    landing-page

--- simple-login-page/script.js
+++ simple-login-page/script.js
@@ -1,9 +1,8 @@
 function handleSubmit(event) {
-    event.preventDefault(); // Prevent the default form submission
-
+    event.preventDefault(); 
+    // Prevent the default form submission
     // Here you can add your sign-in logic (e.g., validation, API call)
     // For demonstration, let's assume the sign-in is always successful.
-
     // Redirect to the desired page after successful sign-in
     window.location.href = 'home.html'; // Change 'home.html' to your target page
 }
